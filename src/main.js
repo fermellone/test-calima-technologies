8c1cb787e10ab902e4cb6209eeeb781111f7c329
femellone <fernandomellone2@gmail.com>
2021-06-08 15:33:26 -0400

    General: Se agregó el filtro "decode" a la instancia de Vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { decode } from "./plugins/filters.js";
 
 Vue.config.productionTip = false;
+
+Vue.filter("decode", decode);
 
 new Vue({
   render: (h) => h(App),
