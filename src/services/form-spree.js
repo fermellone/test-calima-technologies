4514a9a9aac703cdba715811228303982f54636e
femellone <fernandomellone2@gmail.com>
2021-06-08 19:41:19 -0400

    Servicios: Se agregaron los servicios para form-spree

---
+++ src/services/form-spree.js
@@ -0,0 +1,24 @@
+export default {
+  request: async ({ url = "", method = "GET", body = {} }) => {
+    const response = await fetch(url, {
+      method: method.toUpperCase(),
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(body),
+    });
+
+    let data = await response.json();
+
+    if (data.error && data.errors[0].field === "email") {
+      throw new Error("Insertar una direción de correo válida.");
+    } else {
+      return { ...data, next: `https://formspree.io${data.next}` };
+    }
+  },
+};
