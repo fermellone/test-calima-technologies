13de0c8cf57635e418ffaca713e6838d7ff988d2
femellone <fernandomellone2@gmail.com>
2021-06-09 01:31:16 -0400

    Se agregó una barra de navegación para visualizar la lista o el formulario

---
+++ src/components/CtNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <span :class="{ selected: view === 'form' }" @click="selected = 'form'"
+      >Formulario</span
+    >
+    |
+    <span :class="{ selected: view === 'list' }" @click="selected = 'list'"
+      >Lista</span
+    >
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "CtNav",
+  props: {
+    view: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      selected: "form",
+    };
+  },
+  watch: {
+    selected(newVal) {
+      this.$emit("update:view", newVal);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  & > nav span {
+    margin: 0 1rem;
+    cursor: pointer;
+    &.selected {
+      font-weight: bold;
+    }
+  }
+}
+</style>
