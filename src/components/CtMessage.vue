afc6aeee92926f591d6c911d84a6d3b901c2fc10
femellone <fernandomellone2@gmail.com>
2021-06-09 01:34:18 -0400

    Se agregó un componente para las cajas de mensajes

---
+++ src/components/CtMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="message" v-html="`<p>${message}</p>`"></div>
+</template>
+
+<script>
+export default {
+  name: "CtMessage",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  .message {
+    width: 70%;
+    margin: auto;
+    min-height: 5rem;
+    display: table;
+
+    & > p {
+      background: lightgray;
+      border-radius: 5px;
+      padding: 0.3rem 0.1rem;
+    }
+  }
+}
+</style>
